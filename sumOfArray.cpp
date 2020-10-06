525e59a21759cf080dfadf43bedf54811fcd4d24
Aman Joshi <58400326+a-maniac@users.noreply.github.com>
2020-10-06 08:32:19 +0530

    DeShaw

---
+++ sumOfArray.cpp
@@ -0,0 +1,46 @@
+//Sum of Array
+
+//Given an integer array Arr[] of size N. The task is to find sum of it.
+
+/*Input:
+N = 4
+Arr[] = {1, 2, 3, 4}
+Output: 10
+Explanation: 1 + 2 + 3 + 4 = 10. */
+
+
+
+
+//User function template for C++
+class Solution{
+public:
+	// function to return sum of elements
+	// in an array of size n
+	int sum(int arr[], int n) {
+	    // code here
+	    int s=0;
+	    for(int i=0;i<n;i++){
+	        s+=arr[i];
+	    }
+	    return s;
+	}
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.sum(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}  // } Driver Code Ends
